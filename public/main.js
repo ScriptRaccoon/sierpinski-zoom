79e49ef211c93038cbb022010923f60b1f97a7d3
Script Raccoon <scriptraccoon@gmail.com>
2022-03-13 17:55:56 +0100

    adjust alpha speed

--- public/main.js
+++ public/main.js
@@ -1,3 +1,5 @@
+// canvas
+
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
@@ -10,9 +12,13 @@ adjustCanvas();
 
 window.addEventListener("resize", adjustCanvas);
 
+// global variables
+
 let scale = 1;
 let depth = 5;
 const speed = 1.006;
+
+// draw loop
 
 function drawLoop() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -26,13 +32,15 @@ function drawLoop() {
 
 drawLoop();
 
+// draw sierpinski carpet
+
 function drawSierpinski(x, y, size, iteration) {
     if (iteration == 0) return;
     for (let i = 0; i <= 2; i++) {
         for (let j = 0; j <= 2; j++) {
             if (i == 1 && j == 1) {
                 if (iteration == 1) {
-                    ctx.globalAlpha = Math.min(scale - 1, 1);
+                    ctx.globalAlpha = Math.min(0.5 * (scale - 1), 1);
                 }
                 ctx.fillRect(
                     x + size / 3,
